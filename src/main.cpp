b4012fec07387570a4b4aff560e50a590de780f5
Nolasco Napoleao <nolascoamadonapoleao@gmail.com>
2023-10-24 18:23:59 +0100

    Problem instructions

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,30 @@
 #include "libsample/MathUtils.h"
 
 int main() {
-  std::cout << MathUtils::random(0, 33);
+  // Support purchase of nft with offchain payment
+
+  /**
+   * 1. Request call to oracle to retrieve the conversion rate for ETH/USD
+   * https://abi.hashex.org/
+   */
+
+  /** 
+    * 2. Request api call to stripe payment invoice
+    * https://stripe.com/docs/api
+    */
+ 
+  /**
+   * 3. Check if the payment amount matches the nft price 
+   * 
+   */
+
+  /**
+   * 4. Publish payment confirmation to the blockchain
+   * 
+   */
+  
+
+  // Payment triggers minting of nft
 
   return 0;
 }
